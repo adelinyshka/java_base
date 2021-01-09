86397335d015b0ea052cd45c1a910014b53b51b0
Adel Dubinnikova <adelinyshka@gmail.com>
2021-01-09 03:42:56 +0300

    fix: 6. Почему наследование - это плохо [#420942]

--- src/ru/job4j/oop/LiquidationProduct.java
+++ src/ru/job4j/oop/LiquidationProduct.java
@@ -2,6 +2,12 @@ package ru.job4j.oop;
 
 public final class LiquidationProduct {
     private Product product;
+    private String name;
+    private int price;
+
+    public Product getProduct() {
+        return this.product;
+    }
 
     public LiquidationProduct(String name, int price) {
         product = new Product(name, price);
--- src/ru/job4j/oop/Shop.java
+++ src/ru/job4j/oop/Shop.java
@@ -1,6 +1,7 @@
 package ru.job4j.oop;
 
 public class Shop {
+
     public static void main(String[] args) {
         Product product = of("Oil", 100);
         System.out.println(product.label());
@@ -8,7 +9,7 @@ public class Shop {
 
     public static Product of(String name, int price) {
         if ("Oil".equals(name)) {
-            return new LiquidationProduct(name, price);
+            return new Product(name, price);
         }
         return new Product(name, price);
     }
